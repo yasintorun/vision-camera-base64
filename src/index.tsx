1cf252123454e75d557d9e477ec2211e0e45dc0c
Yasin Torun <yasintorun4680@gmail.com>
2023-01-04 14:44:28 +0300

    Add frameToBase64 method

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,8 @@
-import { NativeModules, Platform } from 'react-native';
+import type { Frame } from 'react-native-vision-camera';
 
-const LINKING_ERROR =
-  `The package 'vision-camera-base64' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
 
-const VisionCameraBase64 = NativeModules.VisionCameraBase64
-  ? NativeModules.VisionCameraBase64
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return VisionCameraBase64.multiply(a, b);
+export function frameToBase64(frame: Frame): string {
+  'worklet';
+  // @ts-ignore
+  return __frameToBase64(frame);
 }
